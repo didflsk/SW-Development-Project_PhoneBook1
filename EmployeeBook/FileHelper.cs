8176a0bdc7af37f3db59b7766f2f0158d4109e6a
yukbu406 <42500041+yukbu406@users.noreply.github.com>
2018-08-29 20:42:51 -0700

    upload

---
+++ EmployeeBook/FileHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace EmployeeBook
+{
+    public static class FileHelper
+    {
+        /// <summary>
+        /// Write content to the file
+        /// </summary>
+        /// <param name="filename">Name of the file</param>
+        /// <param name="content">Content to write to the file</param>
+        public static async void WriteTextFileAsync(string filename, 
+            string content)
+        {
+            var localFolder = ApplicationData.Current.LocalFolder;
+            var textFile = await localFolder.CreateFileAsync
+                (filename, CreationCollisionOption.OpenIfExists);
+
+            var textStream = await textFile.OpenAsync(FileAccessMode.ReadWrite);
+            var textWriter = new DataWriter(textStream);
+            textWriter.WriteString(content);
+            await textWriter.StoreAsync();
+        }
+
+        public static async Task<string> ReadTextFileAsync(string filename)
+        {
+            var localFolder = ApplicationData.Current.LocalFolder;
+            var textFile = await localFolder.GetFileAsync(filename);
+            var textStream = await textFile.OpenReadAsync();
+            var textReader = new DataReader(textStream);
+            var textLength = textStream.Size;
+            await textReader.LoadAsync((uint)textLength);
+            return textReader.ReadString((uint)textLength);
+        }
+    }
+}
