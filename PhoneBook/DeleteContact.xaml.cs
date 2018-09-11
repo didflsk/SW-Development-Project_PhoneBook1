a2b1cd20d36b742e1f7e89a59a5b9a14c8fc26bb
Chetana Adhikari <chetana.adhikariKalacademy@outlook.com>
2018-09-11 01:02:04 -0700

    The Delete Button And The Edit Button Features Have Been Incorporated!!! =)

---
+++ PhoneBook/DeleteContact.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PhoneBook
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DeleteContact : Page
+    {
+        public DeleteContact()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            string Firstname = First_Name.Text;
+            string Lastname = Last_Name.Text;
+            Contact.DeleteContactAsync(Firstname, Lastname);
+            var dialog = new MessageDialog("Contact Has Been Deleted");
+
+            await dialog.ShowAsync();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void Back_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
